f524cdef534d1bc0b7f99e3b7fa77a6a8a3c139a
Ralf <ralfferreira995@gmail.com>
2022-01-14 14:32:15 -0300

    Add screen resolution exercise from Exercises 4

---
+++ ResolucaoTela.java
@@ -0,0 +1,12 @@
+package com.ralf;
+
+import java.awt.*;
+
+public class ResolucaoTela {
+    public static void main(String[] args) {
+        Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
+        int lar = (int) tela.getWidth();
+        int alt = (int) tela.getHeight();
+        System.out.println("Sua tela tem resolução " + lar + " x " + alt);
+    }
+}
